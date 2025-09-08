bf7553a2cf351cc6d73f34db5df5a6f5f2c43377
Luiz Gustavo Gouvea de Paula <0220832421024@fatectq.local>
2025-09-08 12:20:21 -0300

    Aitividades 08/09/2025

---
+++ Exemplo Função/biblioteca.js
@@ -0,0 +1,19 @@
+function soma(p1, p2) {
+    var res = p1 + p2;
+    return res
+}
+
+function subtrair(p1, p2) {
+    var res = p1 - p2;
+    document.write("<br>" + `Subtração: ${v1} - ${v2} = ${res}`)
+}
+
+function multi(p1, p2) {
+    var res = p1 * p2;
+    return res
+}
+
+function divi(p1, p2) {
+    var res = p1 / p2;
+    return res
+}
